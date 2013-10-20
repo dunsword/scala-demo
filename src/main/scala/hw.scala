965766f6120131d7844789e0e8c00ef8ee199df3
Paul <dunsword@163.com>
2013-10-20 22:42:02 +0800

    init

---
+++ src/main/scala/hw.scala
@@ -0,0 +1,12 @@
+package scala
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: XPS
+ * Date: 13-10-20
+ * Time: 下午10:17
+ * To change this template use File | Settings | File Templates.
+ */
+object Hi {
+  def main(args: Array[String]) = println("Hi!")
+}
